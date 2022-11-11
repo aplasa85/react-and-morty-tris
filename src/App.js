cce20d8ba942a34e5e95c5a1916aa649a1fcc752
Axel Plasa <aplasa@gmail.com>
2022-11-11 11:07:35 +0100

    Convert html in components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,91 @@
 import './App.css';
+import styled from 'styled-components';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="Navbar-top">
-        <h1>
+    <body >
+      <StyledHeader>
+        <StyledTitle><h1>
           React and Morty App
-          </h1>
-          </div>
-
-          <article className= "card">
+          </h1></StyledTitle>
+         
+          </StyledHeader>
+          <StyledCard>
+          <h3>Random card</h3>  <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" alt="Random Pic"></img>
+          <h3>Random card</h3>  <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" alt="Random Pic"></img>
+          </StyledCard>
+          
+          
+          <Footer>
+            <StyledList>
+              <a href="#"><Link>Link 1</Link></a>
+              <a href="#"><Link>Link 2</Link></a>
+              <a href="#"><Link>Link 3</Link></a>
             
-            <h3>Random card</h3>  <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" alt="Random Pic"></img>
-          </article>
-          <div>
-            <ul className="Navbar-bottom">
-              <li className="Link">Random 1</li>
-              <li className="Link">Random 2</li>
-              <li className="Link">Random 3</li>
-            
-            </ul>
-          </div>
-      </header>
-    </div>
+            </StyledList>
+          
+          </Footer>
+    
+    </body>
   );
 }
+
+
+
+const StyledTitle = styled.h1 `
+  background-color: black;
+  width: 100%;
+  font-family: fantasy;
+  padding: 1px;
+  font-size:smaller;
+  ;
+
+`
+const StyledCard = styled.article `
+background-color: darkgrey;
+margin-bottom: 30px;
+margin-left: 60px;
+border: 1px solid black;
+
+
+`
+
+const StyledHeader =styled.header `
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+const Footer = styled.div `
+background-color: black;
+color: white;
+font-family: fantasy;
+max-width: 100%;
+
+padding: 5px;`
+
+const StyledList = styled.ul `
+color: white;
+border: 1px solid black;
+
+`
+
+
+const Link = styled.li `
+list-style-type: none;
+display: inline;
+color: white;
+padding: 30px;
+
+
+
+
+`
+
+
 
 export default App;
